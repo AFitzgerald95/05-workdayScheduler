f5b583d3efacf61d7c5e99c8aad375b6168e6c2d
Austin Fitzgerald <132597919+AFitzgerald95@users.noreply.github.com>
2024-01-09 18:45:06 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,21 +3,15 @@
 // in the html.
 $(document).ready()
 
-var currentDate = dayjs().format('dddd, MMMM DD, YYYY')
-$('.currentDate').text(currentDate)
+printSchedule();
 
-// console.log(currentDate)
+
+var currentDate = dayjs().format('dddd, MMMM DD, YYYY')
+   $('.currentDate').text(currentDate)
+   // console.log(currentDate)
 
 var currentHour = dayjs().format('HH');
-
-// console.log(currentHour)
-
-var printTime = ('hour-' + currentHour)
-var timeArry = ['9','10','11','12','13','14','15','16','17']
-
-// console.log(printTime)
-// console.log(timeArry)
-
+  // console.log(currentHour)
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -27,10 +21,13 @@ var timeArry = ['9','10','11','12','13','14','15','16','17']
   // useful when saving the description in local storage?
 
   $('.saveBtn').click(function() {
-    var parentId = $(this).parent().attr('id');
-    var userInput = $('textarea').val();
-    localStorage.setItem(parentId, userInput);
+
+    $('.time-block').each(function() {
+      var parentId = $(this).attr('id');
+      var userInput = $(this).find('textarea').val();
+      localStorage.setItem(parentId, userInput);  
   });
+});
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -38,11 +35,41 @@ var timeArry = ['9','10','11','12','13','14','15','16','17']
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
+function timeOfDay() {
 
+  $('.time-block').each(function() {
+    var listTime = parseInt($(this).attr('id').split('-')[1], 10);
+  // console.log(listTime)
+  // var currentHour = dayjs().format('HH');
+  // console.log(currentHour)
+
+    if (listTime === currentHour) {
+      $(this).removeClass('past present future').addClass('present');
+    } else if (listTime > currentHour) {
+      $(this).removeClass('past present future').addClass('future');
+    } else if (listTime < currentHour) {
+      $(this).removeClass('past present future').addClass('past');
+   };
+  });
+};
+
+timeOfDay();
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
+function printSchedule() {
+  $('time-block').each(function() {
+    var parentId = $(this).attr('id');
+    var storedUserInput = localStorage.getItem(parentId);
+    
+    if (storedUserInput) {
+      $(this).find('textarea').val(storedUserInput);
+    }
+  });
+}
+
+  printSchedule();
 
   // TODO: Add code to display the current date in the header of the page.
